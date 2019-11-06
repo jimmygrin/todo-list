b71895be568cd36b6913d0ed659bc76ea09bde4c
Jimmy Lin <jimmydlin@gmail.com>
2019-11-06 08:54:02 -0800

    v1

---
+++ src/reducers/exampleReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  example: ["data"]
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    // actions
+    default:
+      return state
+  }
+}
